1e6d2d0ebab1c8116254f7e3f6a6b942dc1da6f6
Adnane <56194677+AdnaneBennani@users.noreply.github.com>
2026-06-17 16:05:34 +0100

    Setuping Folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    <h1 className="text-3xl font-bold text-red-600">
-      Tailwind fonctionne 🎉
-    </h1>
+        <h1 className="text-3xl font-bold text-red-600">
+          Tailwind fonctionne 🎉
+        </h1>
       </header>
     </div>
   );
